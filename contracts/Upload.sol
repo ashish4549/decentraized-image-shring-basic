63e8c85913b3be0ff018e5400219c420a94547c0
ashish4549 <129039812+ashish4549@users.noreply.github.com>
2024-01-16 00:23:21 +0530

    Create Upload.sol

---
+++ contracts/Upload.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Upload {
+  //create struct Access with user address and their access
+  
+  mapping(address=>string[]) value;
+  mapping(address=>mapping(address=>bool)) ownership;
+  mapping(address=>Access[]) accessList;
+  mapping(address=>mapping(address=>bool)) previousData;
+
+  //create add function that need user address and image url and store into value mapping
+
+  
+  function allow(address user) external {//def
+      ownership[msg.sender][user]=true; 
+      if(previousData[msg.sender][user]){
+         for(uint i=0;i<accessList[msg.sender].length;i++){
+             if(accessList[msg.sender][i].user==user){
+                  accessList[msg.sender][i].access=true; 
+             }
+         }
+      }else{
+          accessList[msg.sender].push(Access(user,true));  
+          previousData[msg.sender][user]=true;  
+      }
+    
+  }
+  function disallow(address user) public{
+      ownership[msg.sender][user]=false;
+      for(uint i=0;i<accessList[msg.sender].length;i++){
+          if(accessList[msg.sender][i].user==user){ 
+              accessList[msg.sender][i].access=false;  
+          }
+      }
+  }
+
+  //create display function with input of user address external view check user ==msg.sender or they can access image or not also return value mapping for perticular user
+
+  //create shareAccess() that return access array for perticular user
+  
+}
